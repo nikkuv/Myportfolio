422b0c61a7114511658949896791f996f5b21817
nikkuv <2000vermasaumya18@gmail.com>
2019-07-30 02:23:32 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typedme", {
+    strings: ["Coder.", "UI/UX and Web designer.", "Front-end Web Developer."],
+    typeSpeed : 50,
+    backSpeed : 50,
+    loop: true, // Default value
+  });
